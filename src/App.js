b68ca65bc3a48c5ee2c6b7dbf24bf0fefda26834
Im-Arjun-Gupta <guptaarjun519@gmail.com>
2023-07-05 15:24:41 +0530

    full code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const MockServiceData = [
+  { name: 'Arjun' },
+  { name: 'Vaidik' },
+  { name: 'Harsh' },
+  { name: 'Arjun' },
+  { name: 'Arjun' },
+  { name: 'Arjun' },
+  { name: 'Vaidik' },
+  { name: 'Vaidik' },
+  { name: 'Arjun' },
+  { name: 'Arjun' },
+  { name: 'Arjun' },
+  { name: 'Arjun' },
+  { name: 'Arjun' },
+  { name: 'Arjun' },
+  { name: 'Arjun' },
+];
+
 function App() {
+  const [data, setData] = useState([]);
+
+  let arr = [];
+
+  useEffect(() => {
+    setData(MockServiceData);
+  }, []);
+
+  const getCount = (name) => {
+    return data.filter((item) => item.name === name).length;
+  };
+
+  const getColor = (duplicateCount) => {
+    if (duplicateCount > 0 && duplicateCount < 3) {
+      return 'red';
+    } else if (duplicateCount >= 3 && duplicateCount < 10) {
+      return 'yellow';
+    } else if (duplicateCount >= 10) {
+      return 'green';
+    }
+    return 'white';
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Duplicates</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => (
+          <tr key={index} style={{ background: getColor(getCount(item.name)) }}>
+            <td>{item.name}</td>
+            <td>{getCount(item.name)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
     </div>
   );
 }
